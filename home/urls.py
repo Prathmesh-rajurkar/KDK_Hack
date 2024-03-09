8fbf67405c09451ffd1cdabfd53e669fe3e02995
CodingClubGhriet <codingclubghriet@gmail.com>
2024-03-09 12:52:20 +0530

    added some features

--- home/urls.py
+++ home/urls.py
@@ -9,7 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.sign_in,name='login'),
     path('signup/',views.sign_up,name='signup'),
-    path('signout/',views.sign_out,name='signout'),
+    path('posts/signout/',views.sign_out,name='signout'),
     path('posts/',views.posts,name='posts'),
     # path('roadbuddy/',('^media/(?PC:/Users/asus/Desktop/KDK-Hack/home/templates/home/media.*)$', 'django.views.static.serve',
     #              {'document_root': settings.MEDIA_ROOT}),)
