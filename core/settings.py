369be66c4d8f76a47fc476befe899b004d03fd29
CodingClubGhriet <codingclubghriet@gmail.com>
2024-03-09 08:14:09 +0530

    logo added

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'home', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'home/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -126,3 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTHENTICATION_BACKENDS=(
     'django.contrib.auth.backends.ModelBackend',
 )
+
+# MEDIA_ROOT = 'C:/Users/asus/Desktop/KDK-Hack/home/templates/home/media'
+# MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'home/static'),]
--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path
+# from django.conf.urls import patterns, url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.sign_in,name='login'),
     path('signup/',views.sign_up,name='signup'),
     path('signout/',views.sign_out,name='signout'),
     path('posts/',views.posts,name='posts'),
+    # path('roadbuddy/',('^media/(?PC:/Users/asus/Desktop/KDK-Hack/home/templates/home/media.*)$', 'django.views.static.serve',
+    #              {'document_root': settings.MEDIA_ROOT}),)
     # path('',views.index,name='index'),
 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
